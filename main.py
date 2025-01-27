d6de96d42db5f6af3032557801ea42f65c8e15dc
jw00006 <jacobawilliamson1@gmail.com>
2025-01-26 23:59:06 -0500

    added functionality for reading from a file and printing the total word count

---
+++ main.py
@@ -0,0 +1,18 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = count_words(text)
+    print(f"There are {num_words} words in the book.")
+    
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+
+main()
