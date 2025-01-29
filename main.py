b82cf6758d6a54c3a8d3e716e10fecde8f63f7d6
jw00006 <jacobawilliamson1@gmail.com>
2025-01-28 19:12:54 -0500

    Now counts individual characters with a dictionary, 'String

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ def main():
     text = get_book_text(book_path)
     num_words = count_words(text)
     print(f"There are {num_words} words in the book.")
+    num_chars = count_characters(text)
+    print(f"There are {num_chars} in the book.")
     
 
 def get_book_text(path):
@@ -13,6 +15,17 @@ def get_book_text(path):
 def count_words(text):
     words = text.split()
     return len(words)
+
+
+def count_characters(text):
+    text = text.lower()
+    char_list = {}
+    for char in text:
+        if char.isalpha() or char == " ":
+            if char not in char_list:
+                char_list[char] = 0
+            char_list[char] += 1
+    return char_list
 
 
 main()
